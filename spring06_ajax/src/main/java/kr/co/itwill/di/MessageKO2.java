5dcfac4584b6ffddcfd3eac4130714869f3633e4
Eunju Jeong <zaoljkr@gmail.com>
2022-11-08 17:56:46 +0900

    di에 대해서

---
+++ spring06_ajax/src/main/java/kr/co/itwill/di/MessageKO2.java
@@ -0,0 +1,15 @@
+package kr.co.itwill.di;
+
+import javax.swing.JOptionPane;
+
+public class MessageKO2 implements IHello{
+	public MessageKO2() {
+		System.out.println("MessageKO2() 객체 생성");
+	}
+	
+	@Override
+	public void sayHello(String name) {
+		JOptionPane.showMessageDialog(null, "Hello~~"+name);		
+	}
+	
+}//class end
