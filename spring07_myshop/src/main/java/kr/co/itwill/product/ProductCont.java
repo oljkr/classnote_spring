492fd7393bd79004a76ba5ba8cc2e9392ebbf77a
Eunju Jeong <zaoljkr@gmail.com>
2022-11-11 16:17:08 +0900

    상품 상세보기, 수정, 삭제 기능 구현

--- spring07_myshop/src/main/java/kr/co/itwill/product/ProductCont.java
+++ spring07_myshop/src/main/java/kr/co/itwill/product/ProductCont.java
@@ -1,8 +1,17 @@
 package kr.co.itwill.product;
 
+import java.io.File;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,13 +23,13 @@ public class ProductCont {
 	}//end
 	
 	@Autowired
-	ProductDAO productDAO;
+	ProductDAO productDao;
 	
 	@RequestMapping("/list")
 	public ModelAndView	list() {
 		ModelAndView mav=new ModelAndView();
 		mav.setViewName("product/list");
-		mav.addObject("list", productDAO.list());
+		mav.addObject("list", productDao.list());
 		return mav;
 	}//list() end
 	
@@ -29,5 +38,100 @@ public class ProductCont {
 		return "product/write";
 	}//write() end
 	
+	@RequestMapping("/insert")
+	public String insert(@RequestParam Map<String, Object> map
+					   , @RequestParam MultipartFile img
+					   , HttpServletRequest req) {
+		
+		//파일업로드할때 리네임 되지 않기 때문에 동일 파일명은 업로드 하면 안됨
+		
+		//업로드된 파일을 /storage폴더에 저장
+		String filename="-";
+		long filesize=0;
+		if(img!=null&&!img.isEmpty()) {
+			filename=img.getOriginalFilename();
+			filesize=img.getSize();
+			try {
+				ServletContext application=req.getSession().getServletContext();
+				String path=application.getRealPath("/storage");//실제 물리적인 경로
+				img.transferTo(new File(path+"\\"+filename));   //파일저장
+				
+			}catch (Exception e) {
+				e.printStackTrace();//System.out.println(e);
+			}//try end
+		}//if end
+		
+		map.put("filename", filename);
+		map.put("filesize", filesize);
+		productDao.insert(map);
+		return "redirect:/product/list";
+	}//insert() end
+	
+	@RequestMapping("/search")
+	public ModelAndView search(@RequestParam(defaultValue = "") String product_name) {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("product/list");
+		mav.addObject("list", productDao.search(product_name));
+		mav.addObject("product_name", product_name); //검색어
+		return mav;
+	}//search() end
+	
+	@RequestMapping("/detail/{product_code}")
+	public ModelAndView detail(@PathVariable String product_code) {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("product/detail");
+		mav.addObject("product", productDao.detail(product_code));
+		return mav;
+	}//detail() end
+	/*
+	@RequestParam
+	http://192.168.0.1:9090?aaa=bbb&ccc=ddd
+	@PathVariable
+	http://192.168.0.1:9090?bbb/ddd
+	*/
+	
+	@RequestMapping("/update")
+	public String update(@RequestParam Map<String, Object> map
+			,@RequestParam MultipartFile img
+			,HttpServletRequest req) {
+		String filename="-";
+		long filesize=0;
+		if(img != null && !img.isEmpty()) {
+			filename = img.getOriginalFilename();
+			filesize=img.getSize();
+			try {
+				ServletContext application=req.getSession().getServletContext();
+				String path=application.getRealPath("/storage");
+				img.transferTo(new File(path+"\\"+filename));
+			}catch (Exception e) {
+				e.printStackTrace();
+			}//try end
+		}else {
+			String product_code=map.get("product_code").toString();
+			Map<String, Object> product=productDao.detail(product_code);
+			filename=product.get("FILENAME").toString();
+			filesize=(long)product.get("FILESIZE");
+		}//if end
+		
+		map.put("filename", filename);
+		map.put("filesize", filesize);
+		productDao.update(map);
+		return "redirect:/product/list";
+	}//update() end
+	
+	@RequestMapping("/delete")
+	public String delete(int product_code, HttpServletRequest req) {
+		String filename=productDao.filename(product_code);//파일삭제
+		if(filename != null && !filename.equals("-")) {
+			ServletContext application=req.getSession().getServletContext();
+			String path=application.getRealPath("/storage");
+			File file=new File(path+"\\"+filename);
+			if(file.exists()) {
+				file.delete();
+			}//if end
+		}//if end
+		productDao.delete(product_code);//행삭제
+		return "redirect:/product/list";
+	}//delete() end
 	
 }//class end
--- spring07_myshop/src/main/java/kr/co/itwill/product/ProductDAO.java
+++ spring07_myshop/src/main/java/kr/co/itwill/product/ProductDAO.java
@@ -22,4 +22,28 @@ public class ProductDAO {
 		return sqlSession.selectList("product.list");
 	}//list() end
 	
+	public void insert(Map<String, Object> map) {
+		sqlSession.insert("product.insert", map);
+	}//insert() end
+	
+	public List<Map<String, Object>> search(String product_name) {
+		return sqlSession.selectList("product.search", "%"+product_name+"%");
+	}//search() end
+	
+	public Map<String, Object> detail(String product_code){
+		return sqlSession.selectOne("product.detail", product_code);
+	}//detail() end
+	
+	public void update(Map<String, Object> map) {
+		sqlSession.update("product.update", map);
+	}//update() end
+	
+	public String filename(int product_code) {
+		return sqlSession.selectOne("product.filename", product_code);
+	}//filename() end
+	
+	public void delete(int product_code) {
+		sqlSession.delete("product.delete", product_code);
+	}//delete() end
+	
 }//class end
